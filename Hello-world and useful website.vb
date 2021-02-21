316966ebd93a7479008bb609b55feabde207c4a0
Megha525 <79240123+Megha525@users.noreply.github.com>
2021-02-21 13:34:30 +0530

    Add files via upload
    This is a visual basic console application to print hello world and open an useful website that every user knows...or may not I don't really care about that...If this project really helped you then fine...else...Okay leave....

---
+++ Hello-world and useful website.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.BackgroundColor = ConsoleColor.Black
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("To you, it can be anyone I don't care.And the website that am opening is very useful and you can download any(almost any) paid softwares from here without paying if you are a windows user...or mac I don't care")
+        Process.Start("Https://www.getintopc.com")
+        Console.ForegroundColor = ConsoleColor.DarkGray
+        Console.Read()
+    End Sub
+
+End Module
